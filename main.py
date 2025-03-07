c790d82bd149a54511c03bfc790df8c6bbd247b3
Kaden Sung <kaden.k.sung@gmail.com>
2025-03-06 18:45:10 -0800

    game over and scoreboard

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from Pipe import Pipe
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
+font100 = pygame.font.SysFont("Arial", 100)
+font30 = pygame.font.SysFont("Arial", 30)
 running = True
+
+
+# texts
+text_gameover = font100.render("Game Over", True, "red")
+text_gameover_pos = (640 - text_gameover.get_width()/2, 360 - text_gameover.get_height()/2)
+text_scoreboard = font30.render("0", True, "black")
+text_scoreboard_pos = (1270 - text_scoreboard.get_width(), 10)
 
 # game setup
 bird_alive = True
 started = False
 bird = Bird()
 pipes = [
-    # Pipe(200),
     Pipe(600),
     Pipe(1000),
-    Pipe(1400)
+    Pipe(1400),
+    Pipe(1800),
 ]
+score = 0
 
 while running:
     # poll for events
@@ -37,14 +47,23 @@ while running:
     if bird.collides_with(pipes):
         bird_alive = False
 
-    if bird_alive:
-        bird.draw(screen)
-        if started:
-            bird.apply_velocity()
     for pipe in pipes:
         pipe.draw(screen)
         if started:
             pipe.move()
+
+    if bird_alive:
+        bird.draw(screen)
+        if started:
+            bird.apply_velocity()
+    else:
+        screen.blit(text_gameover, text_gameover_pos)
+    
+    screen.blit(text_scoreboard, text_scoreboard_pos)
+
+    score += 1
+    text_scoreboard = font30.render(str(score), True, "black")
+    text_scoreboard_pos = (1270 - text_scoreboard.get_width(), 10)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
